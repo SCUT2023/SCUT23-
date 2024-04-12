d6dd9fa9f005c0e87edb7f790e69ecf4cb1442b8
juzixiaoren <a2236482268@outlook.com>
2024-04-12 11:53:55 +0800

    增加了动画效果

--- 网页相关/js/main.js
+++ 网页相关/js/main.js
@@ -6,6 +6,22 @@ var hour = new Date().getHours();
 var minute = new Date().getMinutes();
 var day = new Date().getDay();
 var arr = ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'];
-console.log('今天是 '+ year + '年' + month + '月' + date + '日 ' + hour +':' + minute + ' ' + arr[day]);
+var arr_2 =['结束亦是开始','不要忘记自己的初心','周二得吃塔斯汀','梦……你会有怎样一个梦呢？','疯狂疯狂星期四','再坚持一天','我们只需要进化就好。'];
+if(minute < 10){
+minute = '0' + minute;
+}
 document.getElementById("time").innerHTML = '今天是 '+ year + '年' + month + '月' + date + '日 ' + hour +':' + minute + ' ' + arr[day];
+document.getElementById("tips").innerHTML = 'tips: '+ arr_2[day];
+}
+var temp = 0;
+function replaceimgamiya(){
+    if(temp == 0){
+        document.getElementById("amiyaimg").src="./img/amiya2.gif";
+        temp = 1;
+        setTimeout (replaceimgamiya2, 60000)
+    }
+}
+function replaceimgamiya2(){
+    document.getElementById("amiyaimg").src="./img/amiya.gif";
+    temp = 0;
 }
